79660314fb73e064b649eeeee27339b37ae804f0
Matteo <mcavalluzzo.dev@gmail.com>
2020-05-10 12:08:20 +0200

    created orders classes

---
+++ src/controllers/ordersController.js
@@ -0,0 +1,14 @@
+
+// creazione dell'ordine
+exports.placeOrder = async (req, res) => {
+
+}
+
+// qui potremmo passare id e order state: consegnato, in consegna
+exports.updateOrder = async (req, res) => {
+
+}
+
+exports.getOrders = async (req, res) => {
+
+}
