6d3d16270f7add2eac4f0605150ab2b8e60d1b97
Matteo Cavalluzzo <mcavalluzzo.dev@gmail.com>
2020-05-14 18:08:19 +0200

    renamed catalog endpoints

--- src/routes/catalogRoutes.js
+++ src/routes/catalogRoutes.js
@@ -2,13 +2,13 @@ module.exports = (app) => {
     const categoryController = require('../controllers/categoryController.js')
     const productController = require('../controllers/productController')
 
-    app.route('/catalog/category')
+    app.route('/catalog/categories')
         .get(categoryController.get_categories)
         .post(categoryController.create_category)
         .put(categoryController.update_category)
         .delete(categoryController.delete_category)
 
-    app.route('/catalog/product')
+    app.route('/catalog/products')
         .get(productController.get_category_products)
         .post(productController.add_product)
         .put(productController.update_product)
