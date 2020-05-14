a229c9fc4e458b498af942af79ce801328ef3536
Luca Giorgetti <lucagiorgit95@gmail.com>
2020-05-15 00:11:21 +0200

    fix crud for categories and products

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -8,10 +8,10 @@ exports.get_category_products = async (req, res) => {
         if (existentCategory) {
             const products = existentCategory.products;
             res.status(201).send({ products });
-            return
+            return;
         } else {
             res.status(404).send({ message: "Category doens't exist" });
-            return
+            return;
         }
     } catch (error) {
         res.status(404).send({ description: error.message });
@@ -30,19 +30,20 @@ exports.add_product_to_category = async (req, res) => {
             ingredients,
         } = req.body;
 
-        const existentCategory = await Category.findOne({_id: category_id});
+        const existentCategory = await Category.findOne({ _id: category_id });
         if (existentCategory) {
             existentCategory.products.push({
                 name,
                 description,
                 image,
                 price,
-                ingredients
+                ingredients,
             });
 
             await existentCategory.save();
+            let asd = existentCategory.products[existentCategory.products.length-1]._id
 
-            res.status(201).send({ message: "Product added" });
+            res.status(201).send({ message: `Product successfully added. Id: ${asd}`});
         } else {
             res.status(404).send({ message: "Category doens't exist" });
         }
@@ -58,46 +59,38 @@ exports.update_product = async (req, res) => {
 
         const productId = req.params.productId;
 
-        await (await Category.find({})).forEach((x) => {
-            const prod = x.findOne({"products._id": productId})
-            console.log(x)
-        })
+        const categories = await Category.find();
+        categories.forEach(async (category) => {
+            category.products.forEach(async (product) => {
+                console.log(product.name);
+                if (product._id == productId) {
+                    let categoryId = category._id;
 
+                    let prod_name = name != null ? name : product.name;
+                    let prod_description =
+                        description != null ? description : product.description;
+                    let prod_image = image != null ? image : product.image;
+                    let prod_price = price != null ? price : product.price;
 
-        res.status(201).send({ message: "Product updated" });
+                    let prod_ingredients =
+                        ingredients != null ? ingredients : product.ingredients;
 
+                    await Category.updateOne(
+                        { _id: categoryId, "products._id": productId },
+                        {
+                            "products.$.name": prod_name,
+                            "products.$.description": prod_description,
+                            "products.$.image": prod_image,
+                            "products.$.price": prod_price,
+                            "products.$.ingredients": prod_ingredients,
+                        }
+                    );
 
-        /*
-
-        if (existingCategory) {
-            console.log(category_name);
-            console.log(product_name);
-
-            let product = await Category.findOne({
-                name: category_name,
-                "products._id": productId,
+                    res.status(201).send({ message: "Product updated" });
+                    return;
+                }
             });
-            console.log(product);
-            if (await Category.findOne({name: category_name,"products._id": productId,})) {
-                // TODO: in questo modo sovrascrive TUTTI i campi se non li passo
-                await Category.updateOne(
-                    { name: category_name, "products._id": productId },
-                    {
-                        "products.$.name": name,
-                        "products.$.description": description,
-                        "products.$.image": image,
-                        "products.$.price": price,
-                        "products.$.ingredients": ingredients,
-                    }
-                );
-
-                res.status(201).send({ message: "Product updated" });
-            } else {
-                res.status(404).send({ message: "Product doens't exist" });
-            }
-        } else {
-            res.status(404).send({ message: "Category doens't exist" });
-        }*/
+        });
     } catch (error) {
         res.status(404).send({ description: error.message });
         console.log(error.message);
@@ -106,35 +99,21 @@ exports.update_product = async (req, res) => {
 
 exports.delete_product = async (req, res) => {
     try {
-        const { category_name, product_name } = req.body;
+        const productId = req.params.productId;
 
-        const existingCategory = await Category.findOne({
-            name: category_name,
-        });
-        if (existingCategory) {
-            console.log(category_name);
-            console.log(product_name);
-
-            if (
-                await Category.findOne({
-                    name: category_name,
-                    "products.name": product_name,
-                })
-            ) {
-                // TODO: in questo modo sovrascrive TUTTI i campi se non li passo
-                existingCategory.products = existingCategory.products.filter(
-                    (x) => x.name !== product_name
-                );
-
-                await existingCategory.save();
-
-                res.status(201).send({ message: "Product deleted" });
-            } else {
-                res.status(404).send({ message: "Product doens't exist" });
-            }
-        } else {
-            res.status(404).send({ message: "Category doens't exist" });
-        }
+        const categories = await Category.find()
+        categories.forEach(async (category) => {
+            category.products.forEach(async (product) => {
+                console.log(product.name);
+                if (product._id == productId) {
+                    category.products.id(productId).remove()
+                    category.save()
+         
+                    res.status(201).send({ message: "Product deleted" })
+                    return
+                }
+            })
+        })
     } catch (error) {
         res.status(404).send({ description: error.message });
         console.log(error.message);
