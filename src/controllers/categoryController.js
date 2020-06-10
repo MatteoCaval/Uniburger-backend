7da24dc0d2fc076e760cc41f3be147d3d994192b
Unknown <matteo.cavalluzzo@gmail.com>
2020-06-10 17:42:28 +0200

    fix indentation

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -1,87 +1,87 @@
 const Category = require("../models/categoryModel");
 
 exports.get_categories = async (req, res) => {
-	try {
-		const categories = await Category.find({});
-		res.status(201).send(categories.map(category => {
-			return {
-				id: category._id,
-				name: category.name,
-				image: category.image
-			}
-		}));
-	} catch (error) {
-		console.log(error.message);
-		res.status(400).send({ description: error.message });
-	}
+    try {
+        const categories = await Category.find({});
+        res.status(201).send(categories.map(category => {
+            return {
+                id: category._id,
+                name: category.name,
+                image: category.image
+            }
+        }));
+    } catch (error) {
+        console.log(error.message);
+        res.status(400).send({ description: error.message });
+    }
 };
 
 exports.create_category = async (req, res) => {
-	try {
-		const { name, image } = req.body;
-		console.log(req.body);
+    try {
+        const { name, image } = req.body;
+        console.log(req.body);
 
-		if (name == null) {
-			res.status(401).send({ message: "Wrong parameters" });
-			return
-		}
+        if (name == null) {
+            res.status(401).send({ message: "Wrong parameters" });
+            return
+        }
 
-		const existingCategory = await Category.findOne({ name });
-		if (existingCategory) {
-			res.status(400).send({ description: "Category already present" });
-			return
-		}
+        const existingCategory = await Category.findOne({ name });
+        if (existingCategory) {
+            res.status(400).send({ description: "Category already present" });
+            return
+        }
 
-		const category = new Category({
-			name,
-			image,
-		});
+        const category = new Category({
+            name,
+            image,
+        });
 
-		await category.save();
+        await category.save();
 
-		res.status(201).send({ message: `Category successfully created. Id: ${category._id}` });
-	} catch (error) {
-		console.log(error.message);
-		res.status(400).send({ description: error.message });
-	}
+        res.status(201).send({ message: `Category successfully created. Id: ${category._id}` });
+    } catch (error) {
+        console.log(error.message);
+        res.status(400).send({ description: error.message });
+    }
 };
 // TODO aggiornare prodotti
 exports.update_category = async (req, res) => {
-	try {
-		const categoryId = req.params.categoryId;
-		const { name, image } = req.body;
-		console.log(req.body);
+    try {
+        const categoryId = req.params.categoryId;
+        const { name, image } = req.body;
+        console.log(req.body);
 
-		const existentCategory = await Category.findOne({ _id: categoryId });
-		if (existentCategory) {
-			await Category.updateOne({ _id: categoryId }, { name: name, image: image });
+        const existentCategory = await Category.findOne({ _id: categoryId });
+        if (existentCategory) {
+            await Category.updateOne({ _id: categoryId }, { name: name, image: image });
 
-			res.status(201).send({ message: "Category successfully updated" });
-		} else {
-			res.status(400).send({ description: "Category doesn't exist" });
-		}
-	} catch (error) {
-		console.log(error.message);
-		res.status(400).send({ description: error.message });
-	}
+            res.status(201).send({ message: "Category successfully updated" });
+        } else {
+            res.status(400).send({ description: "Category doesn't exist" });
+        }
+    } catch (error) {
+        console.log(error.message);
+        res.status(400).send({ description: error.message });
+    }
 };
 
 // TODO aggiornare prodotti
 exports.delete_category = async (req, res) => {
-	try {
-		const categoryId = req.params.categoryId;
-		console.log(req.body);
+    try {
+        const categoryId = req.params.categoryId;
+        console.log(req.body);
 
-		const existentCategory = await Category.findOne({ _id: categoryId })
+        const existentCategory = await Category.findOne({ _id: categoryId })
 
-		if (existentCategory) {
-			await Category.deleteOne(existentCategory);
-			res.status(201).send({ message: "Category successfully deleted" });
-		} else {
-			res.status(400).send({ description: "Category doesn't exist" });
-		}
-	} catch (error) {
-		console.log(error);
-		res.status(400).send({ description: error.message });
-	}
+        if (existentCategory) {
+            await Category.deleteOne(existentCategory);
+            res.status(201).send({ message: "Category successfully deleted" });
+        } else {
+            res.status(400).send({ description: "Category doesn't exist" });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(400).send({ description: error.message });
+    }
 };
