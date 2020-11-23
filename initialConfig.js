9d5a5f3bf230cede3409f4324c1f079238585cd4
Luca Giorgetti <lucagiorgit95@gmail.com>
2020-11-23 10:42:35 +0100

    Add empty timetable creation in initial config

--- initialConfig.js
+++ initialConfig.js
@@ -1,6 +1,7 @@
 const config = require('./src/config')
 const mongoose = require('mongoose')
 const User = require('./src/models/userModel')
+const TimeTableDay = require('./src/models/timetableDayModel')
 const bcrypt = require('bcryptjs')
 
 mongoose.connect(
@@ -29,6 +30,69 @@ const createAdmin = async () => {
     }
 }
 
+const createTimeTable = async () => {
+    try {
+        const initialTimeTable = [
+            {
+                name: 'Monday',
+                launchOpen: false,
+                dinnerOpen: false
+            },
+            {
+                name: 'Tuesday',
+                launchOpen: false,
+                dinnerOpen: false
+            },
+            {
+                name: 'Wednesday',
+                launchOpen: false,
+                dinnerOpen: false
+            },
+            {
+                name: 'Thursday',
+                launchOpen: false,
+                dinnerOpen: false
+            },
+            {
+                name: 'Friday',
+                launchOpen: false,
+                dinnerOpen: false
+            },
+            {
+                name: 'Saturday',
+                launchOpen: false,
+                dinnerOpen: false
+            },
+            {
+                name: 'Sunday',
+                launchOpen: false,
+                dinnerOpen: false
+            }
+        ]
+
+        await TimeTableDay.deleteMany({})
+
+        const dayList = initialTimeTable && initialTimeTable.map(day => {
+            return new TimeTableDay({
+                name: day.name,
+                launchOpen: day.launchOpen,
+                dinnerOpen: day.dinnerOpen,
+                ...(day.launchOpen && { launch: { ...day.launch } }),
+                ...(day.dinnerOpen && { dinner: { ...day.dinner } })
+            })
+        })
+
+        await TimeTableDay.insertMany(dayList)
+    } catch (e) {
+        console.log(e)
+    }
+
+}
+
 createAdmin()
-    .then(result => console.log('admin created'))
+    .then(() => console.log('admin created'))
+    .catch(e => console.log(e.message))
+
+createTimeTable()
+    .then(() => console.log('timetable created'))
     .catch(e => console.log(e.message))
